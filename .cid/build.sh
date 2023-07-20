331a9a805cbb73b5d7a70020e1258ad1dbee97e3
huaweicloud <huaweicloud@email.com>
2023-07-20 11:48:37 +0800

    Match-id-e97b4dd0c7de330a3cb58d5fcbb41a2dc78332f4

--- .cid/build.sh
+++ .cid/build.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env bash
-
-CURDIR=$(cd 'dirname $0';pwd)
-
-echo "=======================build.sh========================="
-
-mvn -Dmaven.test.skip=true clean install
+mvn -Dmaven.test.skip=true clean install -Pjar
